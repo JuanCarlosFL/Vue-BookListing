d9f2c5f750f375735a21f0fa1ea3702e4fae4af2
JuanCarlosFL <jcfmalaga@gmail.com>
2021-01-24 20:37:23 +0100

    BookItem created

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.title">{{ book.title }}: {{ book.author }}</li>
+
     </ul>
   </div>
 </template>
@@ -33,8 +33,5 @@ ul{
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: block;
-  margin: 0 10px;
-}
+
 </style>
